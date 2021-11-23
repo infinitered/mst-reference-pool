715f7f616b4cfa2aed74ee8e99f6d941f6e91b28
Jamon Holmgren <jamonholmgren@gmail.com>
2021-11-22 17:23:14 -0800

    Fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
+  preset: "ts-jest",
+  testEnvironment: "node",
+  // only run tests in ./src
+  testPathIgnorePatterns: ["/node_modules/", "/build/"],
+}
